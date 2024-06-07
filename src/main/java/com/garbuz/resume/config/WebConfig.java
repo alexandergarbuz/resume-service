fa9591a4957656fdd1a84a8b7edd6a93f24bde1d
alexandergarbuz <alexander.garbuz@gmail.com>
2024-06-06 19:40:52 -0500

    Initial checkin

---
+++ src/main/java/com/garbuz/resume/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.garbuz.resume.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Bean
+	@Description("Spring Message Resolver")
+	public ResourceBundleMessageSource messageSource() {
+	    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+	    messageSource.setBasename("messages");
+	    return messageSource;
+	}
+	
+	public void addViewControllers(ViewControllerRegistry registry) {
+		//registry.addViewController("/").setViewName("resumePage");
+		registry.addRedirectViewController("/", "/init/data");
+	}
+}
