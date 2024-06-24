9420b3d848125fda50e2c54a3d4b0b900dcb009f
alexandergarbuz <alexander.garbuz@gmail.com>
2024-06-24 12:48:30 -0500

    Enabled CORS policy to allow request from React front end

--- src/main/java/com/garbuz/resume/controller/ResumeWebServiceController.java
+++ src/main/java/com/garbuz/resume/controller/ResumeWebServiceController.java
@@ -7,9 +7,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RestController
 @RequestMapping("/api/resume")
 @Tag(name = "Resume Management System", description = "Operations pertaining to resumes in the Resume Management System")
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE })
 public class ResumeWebServiceController {
 
     private static final Logger LOG = LoggerFactory.getLogger(ResumeWebServiceController.class);
