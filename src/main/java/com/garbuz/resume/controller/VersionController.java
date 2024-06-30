cc0260f77a99380722e0346880fd58caa0f9a7a0
alexandergarbuz <alexander.garbuz@gmail.com>
2024-06-30 15:28:18 -0500

    Removed git version maven plug-in and replaced it with custom code based on GitHub action

--- src/main/java/com/garbuz/resume/controller/VersionController.java
+++ src/main/java/com/garbuz/resume/controller/VersionController.java
@@ -2,29 +2,34 @@ package com.garbuz.resume.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import com.garbuz.resume.util.PropertiesReader;
 
 	
 @Controller
 public class VersionController {
 	private static final Logger LOG = LoggerFactory.getLogger(VersionController.class);
 	
-    @Value("${git.commit.id}")
-    private String gitCommitId;
+    @Autowired
+    private PropertiesReader propertyReader;
     
     
 	@GetMapping("/version")
 	public ModelAndView showVersion() {
-		LOG.info("Show version {}", gitCommitId);
+		String gitCommitSha = this.propertyReader.getCommitSha();
+		String gitCommitComment = this.propertyReader.getCommitComment();		
+		LOG.info("Show version {}", gitCommitSha);
+		LOG.info("Show versioncomment {}", gitCommitComment);
 		ModelAndView mv = new ModelAndView();
 		String viewName = "resumePage";
 		mv.setViewName(viewName);
 		mv.getModel().put(UIConstants.DEFAULT_TEMPLATE, UIConstants.VERSION_FRAGMENT);
 
-		mv.addObject("gitCommitId", gitCommitId);
+		mv.addObject("gitCommitSha", gitCommitSha);
+		mv.addObject("gitCommitComment", gitCommitComment);
 
 		return mv;
 	}
