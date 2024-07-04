ea73c8107474176ca865bb892617169162d5e152
alexandergarbuz <alexander.garbuz@gmail.com>
2024-07-03 20:20:50 -0500

    Added Logging

--- src/main/java/com/garbuz/resume/controller/PdfController.java
+++ src/main/java/com/garbuz/resume/controller/PdfController.java
@@ -2,8 +2,10 @@ package com.garbuz.resume.controller;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,8 @@ import com.garbuz.resume.service.ResumeService;
 @Controller
 public class PdfController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(PdfController.class);
+	
 	@Autowired
     private PdfConvertor pdfConvertor;
 	@Autowired
@@ -24,15 +28,18 @@ public class PdfController {
     @GetMapping("/download-resume-pdf")
     public void downloadResumePdf(@RequestParam("resumeId") Long resumeId, HttpServletResponse response) throws IOException {
         Resume resume = this.resumeService.findResume(resumeId);
+        
+        LOG.debug("Found {}" ,resume);
+        LOG.debug("Found {}", resume.getSkills());
+        
 
         Context context = new Context();
         context.setVariable("resume", resume);
-        context.setVariable(UIConstants.DEFAULT_TEMPLATE, UIConstants.RESUME_FRAGMENT);
 
-        byte[] pdfContent = pdfConvertor.generatePdf("resumePage", context);
+        byte[] pdfContent = pdfConvertor.generatePdf("resumePagePdf", context);
 
         response.setContentType("application/pdf");
-        response.setHeader("Content-Disposition", "attachment; filename=resume.pdf");
+        response.setHeader("Content-Disposition", "attachment; filename=" + resume.getFirstName() + "-" + resume.getLastName() +  "-Resume.pdf");
         response.getOutputStream().write(pdfContent);
         response.getOutputStream().flush();
     }
